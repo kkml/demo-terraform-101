9cc175f0151f573119c16b1a05eafe1b0e5f6117
kkml <kamal.koubaa@gmail.com>
2018-06-25 11:45:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
 provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = ""
-}
-
-resource "aws_instance" "web" {
-  # ...
+  # MODIFY this line to look for 1.20.0 or greater
+  version = ">= 1.20.0"
 }
